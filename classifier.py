8c0dad478c00725c3d05cac0fa3ae0b73a1911f8
Marc Villain <marc.villain@epita.fr>
2020-08-04 04:08:32 +0200

    feat(classifier): adding log countdown

--- classifier.py
+++ classifier.py
@@ -32,7 +32,11 @@ class Classifier:
         :return: Dictionnary of classified images.
         """
         output = {}
-        for image in FilesHelper.get_images_in(folder):
+        images = FilesHelper.get_images_in(folder)
+        images_len = len(images)
+        for i, image in enumerate(images):
+            image_count = i + 1
+            logger.info(f"Computing image ({image_count}/{images_len})")
             palette = self.method.get_palette(image)
             for r, g, b in palette:
                 sort_value = self.sorting.get_value_for(r, g, b)
