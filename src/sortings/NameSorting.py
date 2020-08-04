8b7dc0eda3b6fcd94f79675b6682ee1b72996f13
Marc Villain <marc.villain@epita.fr>
2020-08-04 06:49:48 +0200

    fix(NameSorting): duplicates in color names

--- src/sortings/NameSorting.py
+++ src/sortings/NameSorting.py
@@ -3,23 +3,7 @@ from src.sortings import Sorting
 
 class NameSorting(Sorting):
     colors = {
-        "black": (0, 0, 0),
-        "white": (255, 255, 255),
-        "red": (255, 0, 0),
-        "lime": (0, 255, 0),
-        "blue": (0, 0, 255),
-        "yellow": (255, 255, 0),
-        "cyan": (0, 255, 255),
         "magenta": (255, 0, 255),
-        "silver": (192, 192, 192),
-        "grey": (128, 128, 128),
-        "maroon": (128, 0, 0),
-        "olive": (128, 128, 0),
-        "green": (0, 128, 0),
-        "purple": (128, 0, 128),
-        "teal": (0, 128, 128),
-        "navy": (0, 0, 128),
-        "maroon": (128, 0, 0),
         "dark red": (139, 0, 0),
         "brown": (165, 42, 42),
         "firebrick": (178, 34, 34),
