dc8d36c9174437ba554bd2a66dd4454bdef5c31c
Marc Villain <marc.villain@epita.fr>
2020-08-04 04:08:10 +0200

    feat(methods): add Dominant implementation

--- methods/DominantMethod.py
+++ methods/DominantMethod.py
@@ -1,6 +1,8 @@
+from colorthief import ColorThief
 from methods import Method
 
 class DominantMethod(Method):
     def get_palette(self, image):
-        # FIXME
-        return [(255, 255, 255)]
+        color_thief = ColorThief(image)
+        dominant_color = color_thief.get_color(quality=1)
+        return [dominant_color]
