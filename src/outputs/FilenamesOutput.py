20336549e2cfb0a0f675eadfdb6610050c5e2f2c
Marc Villain <marc.villain@epita.fr>
2020-08-04 06:02:47 +0200

    feat(outputs): add Filenames output implementation

--- src/outputs/FilenamesOutput.py
+++ src/outputs/FilenamesOutput.py
@@ -8,5 +8,9 @@ class FilenamesOutput(Output):
         return FilesHelper.create_dir(self.output_path, self.force)
 
     def compute(self, classified):
-        # FIXME
-        pass
+        for name, images in classified.items():
+            for image in images:
+                # Copy image to new location
+                image_name = name + "_" + FilesHelper.basename(image)
+                new_image = FilesHelper.join(self.output_path, image_name)
+                FilesHelper.copy(image, new_image)
